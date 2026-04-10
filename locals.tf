f68af88bdb2592baf432de56d056364cd323b29b
im-Amitto <29653282+im-Amitto@users.noreply.github.com>
2026-04-10 15:19:39 +0530

    chore: update default tags

--- locals.tf
+++ locals.tf
@@ -2,13 +2,14 @@ locals {
   create_vpc      = var.network == ""
   enable_nuon_dns = contains(["1", "true"], var.enable_nuon_dns)
 
-  sanitized_tags = { for k, v in var.tags : replace(k, "/", "-") => v }
+  sanitized_tags = { for k, v in var.tags : replace(replace(k, "/", "-"), ".", "-") => tostring(v) }
   default_tags = merge(local.sanitized_tags, {
-    "install.nuon.co-id"   = var.nuon_id
-    "sandbox.nuon.co-name" = "gcp-min"
+    "nuon-install-id" = var.nuon_id
+    "managed-by"      = "nuon"
+    "sandbox-name"    = "gcp-min"
   })
   labels = merge(
-    { for k, v in var.additional_tags : replace(k, "/", "-") => v },
+    { for k, v in var.additional_tags : replace(replace(k, "/", "-"), ".", "-") => tostring(v) },
     local.default_tags,
   )
 }
