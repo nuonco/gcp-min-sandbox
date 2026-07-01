64551e3d9145099fad4d381aa2551b66878587ae
im-Amitto <29653282+im-Amitto@users.noreply.github.com>
2026-07-01 19:42:48 +0530

    fix: remove cloudresourcemanager from sandbox (enabled by install stack)

--- services.tf
+++ services.tf
@@ -16,9 +16,4 @@ resource "google_project_service" "dns" {
   disable_on_destroy = false
 }
 
-resource "google_project_service" "cloud_resource_manager" {
-  project            = var.project_id
-  service            = "cloudresourcemanager.googleapis.com"
-  disable_on_destroy = false
-}
 
